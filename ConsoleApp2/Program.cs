0a2c4fac68447fd3a4d982529bca3ce5560054cd
Pranto <sabbiryellow@gmail.com>
2021-04-18 17:35:03 +0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+        }
+    }
+}
