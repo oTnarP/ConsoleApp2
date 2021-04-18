8bf59df750613d4fce1972af5e68ca5c377581d7
Pranto <sabbiryellow@gmail.com>
2021-04-18 17:42:47 +0600

    Variables

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,8 +6,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //All Variables
+
+            int age = 22;
+            string name = "Pranto";
+            char eyes = '2';
+            decimal rice = 2.5M;
             DateTime dateTime = DateTime.Now;
-            Console.WriteLine(dateTime);
+
+
+            Console.WriteLine("My name is " + name + " and I am " + age + " years old. I am gonna buy " + rice + " KG Rice and Current Time is : " + dateTime);
         }
     }
 }
